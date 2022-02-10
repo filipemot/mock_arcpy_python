9d206a3b9e9a8aa328e9e94804b438bf7d322ec7
Luis Filipe Guedes Mota <filipemot@gmail.com>
2022-02-10 09:05:24 -0300

    Adjust Tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -11,6 +11,7 @@ class TestMain(unittest.TestCase):
     def test_get_results_feature_found(self, spy_search_cursor):
         spy_search_cursor.return_value = [['Teste1'], ['Teste2']]
         return_values = main.search_results("feature_class")
+        main.arcpy.da.SearchCursor.assert_called_once()
         self.assertEqual(len(return_values), 2)
         self.assertEqual(return_values[0], 'Teste1')
         self.assertEqual(return_values[1], 'Teste2')
